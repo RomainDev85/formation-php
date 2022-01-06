a57468645138c325cbde305a52c8c9e3752d7867
Romain85340 <64895507+Romain85340@users.noreply.github.com>
2022-01-06 10:36:43 +0100

    chapitre 20: dashboard fait

---
+++ html/dashboard.php
@@ -0,0 +1,79 @@
+<?php
+require 'functions/compteur.php';
+$annee = (int)date('Y');
+$annee_selection = empty($_GET['annee']) ? null : (int)$_GET['annee'];
+$mois_selection = empty($_GET['mois']) ? null : $_GET['mois'];
+if($annee_selection && $mois_selection){
+  $total = nombre_vues_mois($mois_selection, $annee_selection);
+  $detail = nombre_vues_detail_mois($mois_selection, $annee_selection);
+} else {
+  $total = nombre_vues();
+};
+$mois = [
+  '01' => 'Janvier',
+  '02' => 'Février',
+  '03' => 'Mars',
+  '04' => 'Avril',
+  '05' => 'Mai',
+  '06' => 'Juin',
+  '07' => 'Juillet',
+  '08' => 'Aout',
+  '09' => 'Septembre',
+  '10' => 'Octobre',
+  '11' => 'Novembre',
+  '12' => 'Décembre'
+];
+require 'components/header.php';
+?>
+
+<div class="row m-4">
+  <div class="col-md-4">
+    <div class="list-group">
+      <?php for($i = 0; $i < 5; $i++): ?>
+        <div class="m-2">
+          <a class="list-group-item <?php echo $annee - $i === $annee_selection ? "active" : "" ?> " href="dashboard.php?annee=<?= $annee - $i ?>"><?= $annee - $i ?></a>
+          <div class="list-group">
+          <?php if($annee - $i === $annee_selection): ?>
+            <?php foreach($mois as $numero => $element): ?>
+              <a href="dashboard.php?annee=<?= $annee_selection ?>&mois=<?= $numero ?>" class="list-group-item <?= $numero === $mois_selection ? 'active' : ''?>"><?= $element ?></a>
+            <?php endforeach ?>
+          <?php endif ?>
+          </div>
+        </div>
+      <?php endfor ?>
+    </div>
+  </div>
+  <div class="col-md-8">
+    <div class="card">
+      <strong style="font-size:3rem;"><?= $total ?></strong>
+      Visite total.
+    </div>
+    <?php if(isset($detail)): ?>
+    <h2 class="mt-4">Details des visites pour le mois</h2>
+    <table class="container-fluid table table-striped">
+      <thead>
+        <tr>
+          <th>Jour</th>
+          <th>Mois</th>
+          <th>Année</th>
+          <th>Nombre visite</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($detail as $ligne): ?>
+        <tr>
+          <td><?= $ligne['jour'] ?></td>
+          <td><?= $ligne['mois'] ?></td>
+          <td><?= $ligne['annee'] ?></td>
+          <td><?= $ligne['visites'] ?> visite<?= $ligne['visites'] > 1 ? 's' : '' ?></td>
+        </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+    <?php endif ?>
+  </div>
+</div>
+
+<?php
+require 'components/footer.php';
+?>
--- html/functions/compteur.php
+++ html/functions/compteur.php
@@ -16,9 +16,35 @@ function incrementer_compteur (string $fichier): void {
   file_put_contents($fichier, $compteur);
 }
 
-
-
 function nombre_vues(): string {
   $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'compteur';
   return file_get_contents($fichier);
+}
+
+function nombre_vues_mois(int $mois, int $annee): int {
+  $mois = str_pad($mois, 2, '0', STR_PAD_LEFT);
+  $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . "compteur-$annee-$mois-*";
+  $fichiers = glob($fichier);
+  $total = 0;
+  foreach($fichiers as $fichier){
+    $total = $total += (int)file_get_contents($fichier);
+  }
+  return $total;
+}
+
+function nombre_vues_detail_mois(int $mois, int $annee): array {
+  $mois = str_pad($mois, 2, '0', STR_PAD_LEFT);
+  $fichier = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . "compteur-$annee-$mois-*";
+  $fichiers = glob($fichier);
+  $visites = [];
+  foreach($fichiers as $fichier){
+    $parties = explode('-', basename($fichier));
+    $visites[] = [
+      'annee' => $parties[1],
+      'mois' => $parties[2],
+      'jour' => $parties[3],
+      'visites' => file_get_contents($fichier)
+    ];
+  }
+  return $visites;
 }
